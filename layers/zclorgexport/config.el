f57b5a7a479c013b2c563ef68517494728a2e44a
Eason <makestemclear@gmail.com>
2020-01-25 21:57:11 +0800

    efficiency: preview the math equaiton
    http://slumpy.org/blog/2017-02-01-automatic-latex-preview-in-org-mode/

--- layers/zclorgexport/config.el
+++ layers/zclorgexport/config.el
@@ -146,7 +146,8 @@
 (setq exec-path (append exec-path '("/usr/local/texlive/2019/bin/x86_64-darwin/")))
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((latex . t)))
+ '((latex . t)
+   (gnuplot . t)))
 
 
 (setq org-image-actual-width 800)
@@ -209,85 +210,90 @@ BEG and END default to the buffer boundaries."
                   (overlay-put ov 'modification-hooks
                                (list 'org-display-inline-remove-overlay))
                   (push ov org-inline-image-overlays))))))))))
+
 (add-hook 'org-mode-hook 'org-display-inline-images)
 
-;; Automatic latex image toggling when cursor is on a fragment
-(defvar org-latex-fragment-last nil
-  "Holds last fragment/environment you were on.")
 
-(defun org-latex-fragment-toggle ()
-  "Toggle a latex fragment image "
-  (interactive)
-  (and (eq 'org-mode major-mode)
-      (let* ((el (org-element-context))
-              (el-type (car el)))
-        (cond
-          ;; were on a fragment and now on a new fragment
-          ((and
-            ;; fragment we were on
-            org-latex-fragment-last
-            ;; and are on a fragment now
-            (or
-            (eq 'latex-fragment el-type)
-            (eq 'latex-environment el-type))
-            ;; but not on the last one this is a little tricky. as you edit the
-            ;; fragment, it is not equal to the last one. We use the begin
-            ;; property which is less likely to change for the comparison.
-            (not (= (org-element-property :begin el)
-                    (org-element-property :begin org-latex-fragment-last))))
-          ;; go back to last one and put image back
-          (save-excursion
-            (goto-char (org-element-property :begin org-latex-fragment-last))
-            (org-preview-latex-fragment))
-          ;; now remove current image
-          (goto-char (org-element-property :begin el))
-          (let ((ov (cl-loop for ov in (org--list-latex-overlays)
-                          if
-                          (and
-                            (<= (overlay-start ov) (point))
-                            (>= (overlay-end ov) (point)))
-                          return ov)))
-            (when ov
-              (delete-overlay ov)))
-          ;; and save new fragment
-          (setq org-latex-fragment-last el))
+ (defvar kk/org-latex-fragment-last nil
+    "Holds last fragment/environment you were on.")
 
-          ;; were on a fragment and now are not on a fragment
-          ((and
-            ;; not on a fragment now
-            (not (or
-                  (eq 'latex-fragment el-type)
-                  (eq 'latex-environment el-type)))
-            ;; but we were on one
-            org-latex-fragment-last)
-          ;; put image back on
-          (save-excursion
-            (goto-char (org-element-property :begin org-latex-fragment-last))
-            (org-preview-latex-fragment))
-          ;; unset last fragment
-          (setq org-latex-fragment-last nil))
+  (defun kk/org-in-latex-fragment-p ()
+    "Return the point where the latex fragment begins, if inside
+  a latex fragment. Else return false"
+    (let* ((el (org-element-context))
+           (el-type (car el)))
+      (and (or (eq 'latex-fragment el-type) (eq 'latex-environment el-type))
+          (org-element-property :begin el))))
 
-          ;; were not on a fragment, and now are
-          ((and
-            ;; we were not one one
-            (not org-latex-fragment-last)
-            ;; but now we are
-            (or
-            (eq 'latex-fragment el-type)
-            (eq 'latex-environment el-type)))
-          (goto-char (org-element-property :begin el))
-          ;; remove image
-          (let ((ov (cl-loop for ov in (org--list-latex-overlays)
-                          if
-                          (and
-                            (<= (overlay-start ov) (point))
-                            (>= (overlay-end ov) (point)))
-                          return ov)))
-            (when ov
-              (delete-overlay ov)))
-          (setq org-latex-fragment-last el))))))
+  (defun kk/org-latex-fragment-toggle ()
+    "Toggle a latex fragment image "
+    (and (eq 'org-mode major-mode)
+	 (let ((begin (kk/org-in-latex-fragment-p)))
+           (cond
+            ;; were on a fragment and now on a new fragment
+            ((and
+              ;; fragment we were on
+              kk/org-latex-fragment-last
+              ;; and are on a fragment now
+              begin
 
+              ;; but not on the last one this is a little tricky. as you edit the
+              ;; fragment, it is not equal to the last one. We use the begin
+              ;; property which is less likely to change for the comparison.
+              (not (and kk/org-latex-fragment-last
+			(= begin
+			   kk/org-latex-fragment-last))))
+             ;; go back to last one and put image back, provided there is still a fragment there
+             (save-excursion
+               (goto-char kk/org-latex-fragment-last)
+               (when (kk/org-in-latex-fragment-p) (org-preview-latex-fragment))
 
-(add-hook 'post-command-hook 'org-latex-fragment-toggle)
+               ;; now remove current image
+               (goto-char begin)
+               (let ((ov (loop for ov in (org--list-latex-overlays)
+                               if
+                               (and
+				(<= (overlay-start ov) (point))
+				(>= (overlay-end ov) (point)))
+                               return ov)))
+		 (when ov
+                   (delete-overlay ov)))
+               ;; and save new fragment
+               (setq kk/org-latex-fragment-last begin)))
+
+            ;; were on a fragment and now are not on a fragment
+            ((and
+              ;; not on a fragment now
+              (not begin)
+              ;; but we were on one
+              kk/org-latex-fragment-last)
+             ;; put image back on, provided that there is still a fragment here.
+             (save-excursion
+               (goto-char kk/org-latex-fragment-last)
+               (when (kk/org-in-latex-fragment-p) (org-preview-latex-fragment)))
+
+             ;; unset last fragment
+             (setq kk/org-latex-fragment-last nil))
+
+            ;; were not on a fragment, and now are
+            ((and
+              ;; we were not one one
+              (not kk/org-latex-fragment-last)
+              ;; but now we are
+              begin)
+             ;; remove image
+             (save-excursion
+               (goto-char begin)
+               (let ((ov (loop for ov in (org--list-latex-overlays)
+                               if
+                               (and
+				(<= (overlay-start ov) (point))
+				(>= (overlay-end ov) (point)))
+                               return ov)))
+		 (when ov
+                   (delete-overlay ov))))
+             (setq kk/org-latex-fragment-last begin))))))
+
+(add-hook 'post-command-hook 'kk/org-latex-fragment-toggle t)
 
 )
